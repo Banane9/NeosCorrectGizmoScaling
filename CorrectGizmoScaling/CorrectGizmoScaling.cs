a758528e3f4c94bb0bb2fef8c57c41a3d9e7fdaa
Arne Kiesewetter <banane9@live.de>
2022-12-14 16:54:02 +0100

    Add project files.

---
+++ CorrectGizmoScaling/CorrectGizmoScaling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using BaseX;
+using CodeX;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using FrooxEngine.LogiX.Data;
+using FrooxEngine.LogiX.ProgramFlow;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using NeosModLoader;
+
+namespace CorrectGizmoScaling
+{
+    public class CorrectGizmoScaling : NeosMod
+    {
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosCorrectGizmoScaling";
+        public override string Name => "CorrectGizmoScaling";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(MaterialGizmo), "PositionInFrontOfUser")]
+        private static class MaterialGizmoPatch
+        {
+            private static void Prefix(RelayRef<Slot> ____inspectorRoot)
+            {
+                ____inspectorRoot.Target.LocalScale = float3.One;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Correct Gizmo Scaling
+===========================
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that fixes the scaling of the material inspector spawned from the Material Tip.
+
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [CorrectGizmoScaling.dll](https://github.com/Banane9/NeosCorrectGizmoScaling/releases/latest/download/CorrectGizmoScaling.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
